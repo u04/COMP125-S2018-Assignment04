ee497efa60c33390f1d9bbaf45bc1c4d339a9e53
u04 <chowkas@rogers.com>
2018-08-14 20:41:38 -0400

    seems to be working ok

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'About Me' });
+});
+
+router.get('/contact.html', function(req, res, next) { //|||||||||||||||||||| takes the 404 error away if I type /contact at the web address |||||||||||||||||||||||||||||||||||
+  res.render('contact', { title: 'Contact' });
+});
+
+router.get('/project.html', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+router.get('/index.html', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
 });
 
 module.exports = router;
